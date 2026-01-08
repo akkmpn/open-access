eeb290275621b15f6e83f0445ac001187bd5bca5
akkmpn <akkmpn@gmail.com>
2026-01-08 20:25:47 +0530

    taskpro js url fix

--- TaskPro/taskpro.js
+++ TaskPro/taskpro.js
@@ -4,7 +4,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
    SUPABASE CONFIGURATION
    ============================================ */
 
-const SUPABASE_URL = "https://rvxcabyylfpzvxpisemb.supabase.co";
+const SUPABASE_URL = "https://ozfmiburxwatrkbhnodw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2eGNhYnl5bGZwenZ4cGlzZW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3ODA5ODAsImV4cCI6MjA4MzM1Njk4MH0.qm6NrMhVyEnoEP7f_wVupd2-hMkq0W7TdVBK7RHu5mg";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
